d58b89281475d916684ae41f0c2c3bf5d681bc63
Goune <71518163+Goune1@users.noreply.github.com>
2023-09-01 14:26:31 +0200

    Add files via upload

--- Admin Dashboard #01/login.js
+++ Admin Dashboard #01/login.js
@@ -5,14 +5,11 @@ submit.addEventListener('click', function() {
     var mdp = password.value;
     console.log(mdp);
     
-    if (mdp === "1234") {
-        // Le mot de passe est correct, vous pouvez effectuer des actions ici
-        // Par exemple, rediriger l'utilisateur vers une autre page ou afficher un message de succès
-        window.location.href("https://gounevps.com/index.html")
+    if (mdp == "1234") {
+        window.location.assign("https://gounevps.com")
         console.log("Mot de passe correct !");
         
     } else {
-        // Le mot de passe est incorrect, vous pouvez afficher un message d'erreur ou prendre d'autres mesures
         console.log("Mot de passe incorrect !");
     }
 });
